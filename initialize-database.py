a08702641f587e7397592c00ab7a28bb2cdf0966
Jeff <jbuck.wx@gmail.com>
2019-01-07 02:01:54 +0000

    add return value to create_climo_table function

--- initialize-database.py
+++ initialize-database.py
@@ -33,13 +33,15 @@ def create_climo_table():
     mariadb_connection = mariadb.connect(user="WeatherPy", password="SnowStorm1991", database="weather")
     cursor = mariadb_connection.cursor()
     
+	status = True
     try:
         cursor.execute("CREATE TABLE IF NOT EXISTS climo_test (climoID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, stationID VARCHAR(12) NOT NULL, date DATETIME NOT NULL, high TINYINT(4) DEFAULT NULL, low TINYINT(4) DEFAULT NULL, qpf DECIMAL(4,2) DEFAULT NULL, snow DECIMAL(3,1) DEFAULT NULL, depth TINYINT(3) DEFAULT NULL, winter DECIMAL(6,2) NOT NULL) ENGINE=Aria DEFAULT CHARSET=latin1;")
     except mariadb.Error as error:
         print("Error: {}".format(error))
+		status = False
 
 
-    return
+    return status
 	
 	
 	
