7bd6472a8ef5549343ac847a9b515d7f0511add7
Jeff <jbuck.wx@gmail.com>
2019-01-20 22:00:09 +0000

    collect-daily-data.py: add function(s): read_url, find_data

--- collect-daily-data.py
+++ collect-daily-data.py
@@ -5,3 +5,55 @@ import urllib.request
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup as bs
 
+
+
+def read_url(url):
+    """
+    Accepts:
+        url     url of webpage
+
+    Returns:
+        text string of webpage or False on error
+    """
+    try:
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        text = data.decode('utf-8')
+    except urllib.error.URLError:
+        print('URL Error')
+        text = False
+    except urllib.error.HTTPError:
+        text = False
+    
+    return text
+
+
+
+def find_data(regex,subject,start=0):
+    """
+    Accepts:
+        regex       re module regex that must contain 1 group labeled 'data'
+                    example: r'MAXIMUM +(?P<max>[\-0-9TM]{1,4})'
+                    
+        subject     string that will be searched
+        
+        start       string position to begin the search.
+        
+    Returns:
+        value of the data group or 'null'
+        string posistion of the end of the match or original start
+        
+    """
+    match = re.search(regex, subject[start:])
+
+    value = 'null'
+    end = start
+    if match:
+        value = match.group('data')
+        end   = match.end('data')
+    else:
+        print('Match not found.')
+        
+    return value, end
+
+
